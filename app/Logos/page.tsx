3ecb1dd26bf4c0ab5be9a9d03ce1371bf39a5944
Royce <coviello@royce.gmail.com>
2023-11-06 15:49:20 -0800

    fixed logos page

--- app/Logos/page.tsx
+++ app/Logos/page.tsx
@@ -1,18 +1,41 @@
-export default function Home() {
-return (
-    <div>
-        <div className="w-[1394px] h-[2598px] relative bg-white bg-opacity-0">
-  <div className="w-[564px] h-[554px] left-[36px] top-[53px] absolute bg-white justify-center items-center inline-flex">
-    <img className="/public/logo1.png" />
-  </div>
-  <img className="w-[564px] h-[554px] left-[36px] top-[1279px] absolute" src="https://via.placeholder.com/564x554" />
-  <img className="w-[554px] h-[554px] left-[798px] top-[53px] absolute" src="https://via.placeholder.com/554x554" />
-  <img className="w-[581px] h-[542px] left-[798px] top-[1279px] absolute" src="https://via.placeholder.com/581x542" />
-  <div className="w-[564px] h-[415px] left-[798px] top-[653px] absolute text-black text-[37px] font-normal font-['Imprima']">The first logo features a compass rose at its center with a prominent directional arrow pointing downwards, integrating a "V" shape that could symbolize "victory" or "valley." It's encircled by two rings that suggest motion or navigation. The color palette consists of different shades of teal, and the background is a light beige. The overall design conveys exploration or guidance.</div>
-  <div className="w-[564px] h-[415px] left-[41px] top-[659px] absolute text-black text-[37px] font-normal font-['Imprima']">The fourth logo is straightforward and clear, featuring a globe enclosed in a circle with dotted lines around it, suggesting a global connection or travel route. Over the globe, there's a banner with the text "WHERE 2 NEXT," indicating a theme of travel and exploration. The entire logo is set against a gradient background that shifts from green to blue, which can be associated with growth, eco-friendliness, or tranquility.</div>
-  <div className="w-[564px] h-[415px] left-[36px] top-[1868px] absolute text-black text-[37px] font-normal font-['Imprima']">The second logo has a more traditional compass design, with the four cardinal directions N, S, E, and W clearly marked and the compass needle in a prominent position. There are also numbers present, perhaps suggesting degrees of bearing. The compass is framed within two concentric circles with decorative elements, and the color scheme includes teal, coral, and a peach background. This logo suggests precision, orientation, and a classic navigation theme.</div>
-  <div className="w-[575px] h-[618px] left-[804px] top-[1872px] absolute text-black text-[37px] font-normal font-['Imprima']">The third logo illustrates a stylized location pin combined with a globe in the center, conveying the idea of global positioning or international travel. The pin is designed with decorative elements like dotted lines and small triangles, which give it a dynamic and detailed appearance. The color scheme is a gradation of teals on a dark blue background, offering a modern and digital feel.</div>
-</div>
+import React from 'react';
+
+const LogoDescriptions = () => {
+  return (
+    <div className="max-w-sm mx-auto">
+      {/* First Logo Description */}
+      <div className="p-6 bg-teal-100 text-teal-900 rounded-lg shadow-md mb-4">
+        <h2 className="text-lg font-semibold mb-2">The first logo</h2>
+        <p className="text-sm">
+          The first logo features a compass rose at its center with a prominent directional arrow pointing downwards, integrating a "V" shape that could symbolize "victory" or "valley." It's encircled by two rings that suggest motion or navigation. The color palette consists of different shades of teal, and the background is a light beige. The overall design conveys exploration or guidance.
+        </p>
+      </div>
+
+      {/* Second Logo Description */}
+      <div className="p-6 bg-blue-100 text-blue-900 rounded-lg shadow-md mb-4">
+        <h2 className="text-lg font-semibold mb-2">The second logo</h2>
+        <p className="text-sm">
+          The second logo has a more traditional compass design, with the four cardinal directions N, S, E, and W clearly marked and the compass needle in a prominent position. There are also numbers present, perhaps suggesting degrees of bearing. The compass is framed within two concentric circles with decorative elements, and the color scheme includes teal, coral, and a peach background. This logo suggests precision, orientation, and a classic navigational theme.
+        </p>
+      </div>
+
+      {/* Third Logo Description */}
+      <div className="p-6 bg-green-100 text-green-900 rounded-lg shadow-md mb-4">
+        <h2 className="text-lg font-semibold mb-2">The third logo</h2>
+        <p className="text-sm">
+          The third logo illustrates a stylized location pin combined with a globe in the center, conveying the idea of global positioning or international travel. The pin is designed with decorative elements like dotted lines and small triangles, which give it a dynamic and detailed appearance. The color scheme is a gradation of teals on a dark blue background, offering a modern and digital feel.
+        </p>
+      </div>
+
+      {/* Fourth Logo Description */}
+      <div className="p-6 bg-green-300 text-green-900 rounded-lg shadow-md">
+        <h2 className="text-lg font-semibold mb-2">The fourth logo</h2>
+        <p className="text-sm">
+          The fourth logo is straightforward and clear, featuring a globe enclosed in a circle with dotted lines around it, suggesting a global connection or travel route. Over the globe, there's a banner with the text "WHERE 2 NEXT," indicating a theme of travel and exploration. The entire logo is set against a gradient background that shifts from green to blue, which can be associated with growth, eco-friendliness, or tranquility.
+        </p>
+      </div>
     </div>
-)
-}
+  );
+};
+
+export default LogoDescriptions;
